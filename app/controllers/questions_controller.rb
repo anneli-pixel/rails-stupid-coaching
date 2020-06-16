951df6a68e00b2eef315417fdf760bc43f1d805f
Anne Lißner <anne_lissner@gmx.net>
2020-06-16 20:12:35 +0200

    completed coaches answers logic and added backlink to ask page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    if params[:question] == 'I am going to work'
+      @answer = 'Great'
+    elsif params[:question].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    @answer
   end
 end
